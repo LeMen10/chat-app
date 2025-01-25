e27e399eb866bfdeeedf5813534dc5d63638583a
menvo <volemenn@gmail.com>
2025-01-25 20:13:00 +0700

    add message ctl, message routes and user routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,9 @@
 import authRoutes from './auth.routes.js';
+import messageRoutes from './message.routes.js';
+import userRoutes from './user.routes.js';
 
 export const route = (app) => {
     app.use('/api/auth', authRoutes);
+    app.use('/api/messages', messageRoutes);
+    app.use('/api/users', userRoutes);
 };

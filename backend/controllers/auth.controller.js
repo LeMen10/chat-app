f0db2a7817f35e508fcf77821b47d6d625de1e80
menvo <volemenn@gmail.com>
2025-06-12 18:34:07 +0700

    update socket

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,7 +6,7 @@ export const signup = async (req, res) => {
     try {
         const { fullName, username, password, confirmPassword, gender } = req.body;
         if (password !== confirmPassword) return res.status(400).json({ error: "Passwords don't match" });
-        
+
         const user = await User.findOne({ username });
         if (user) return res.status(400).json({ error: 'Username already exists' });
 
@@ -24,22 +24,17 @@ export const signup = async (req, res) => {
             profilePic: gender === 'male' ? boyProfilePic : girlProfilePic,
         });
 
-        if (newUser) {
-            // Generate JWT token here
-            generateTokenAndSetCookie(newUser._id, res);
-            await newUser.save();
+        if (!newUser) return res.status(400).json({ error: 'Invalid user data' });
+        generateTokenAndSetCookie(newUser._id, res);
+        await newUser.save();
 
-            res.status(201).json({
-                _id: newUser._id,
-                fullName: newUser.fullName,
-                username: newUser.username,
-                profilePic: newUser.profilePic,
-            });
-        } else {
-            res.status(400).json({ error: 'Invalid user data' });
-        }
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic,
+        });
     } catch (error) {
-        console.log('Error in signup controller', error.message);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
@@ -62,7 +57,6 @@ export const login = async (req, res) => {
             token,
         });
     } catch (error) {
-        console.log('Error in login controller', error.message);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
@@ -72,7 +66,6 @@ export const logout = (req, res) => {
         res.cookie('jwt', '', { maxAge: 0 });
         res.status(200).json({ message: 'Logged out successfully' });
     } catch (error) {
-        console.log('Error in logout controller', error.message);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -10,7 +10,6 @@ export const getUsersForSidebar = async (req, res) => {
         const filteredUsers = await User.find(filter).select("-password");
         res.status(200).json(filteredUsers);
     } catch (error) {
-        console.error("Error in getUsersForSidebar: ", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 };
